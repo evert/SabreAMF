7278db90869b954900259f304bbd3b3538b81fc1
evert <evert@rooftopsolutions.nl>
2006-11-15 02:16:00 +0000

    Added docblocks and fixed typo
    git-svn-id: http://sabreamf.googlecode.com/svn/sabreamf/trunk@106 4d1502ab-f33c-0410-ba90-8f7d5a69d0d2

--- SabreAMF/ByteArray.php
+++ SabreAMF/ByteArray.php
@@ -1,22 +1,53 @@
 <?php
 
+    /**
+     * SabreAMF_ByteArray 
+     * 
+     * @package SabreAMF
+     * @version $Id$
+     * @copyright Copyright (C) 2006 Rooftop Solutions. All rights reserved.
+     * @author Evert Pot (http://www.rooftopsolutions.nl) 
+     * @license licence http://www.freebsd.org/copyright/license.html  BSD License (4 Clause)
+     */
     class SabreAMF_ByteArray {
 
+        /**
+         * data 
+         * 
+         * @var string 
+         */
         private $data;
 
-        function __construct($data = false) {
+        /**
+         * __construct 
+         * 
+         * @param string $data 
+         * @return void
+         */
+        function __construct($data = '') {;
 
             $this->data = $data;
 
         }
 
+        /**
+         * getData 
+         * 
+         * @return string 
+         */
         function getData() {
 
             return $this->data;
 
         }
 
-        function setData() {
+        /**
+         * setData 
+         * 
+         * @param string $data
+         * @return void
+         */
+        function setData($data) {
 
             $this->data = $data;
 
